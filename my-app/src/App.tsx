1f0bf9c89f1de76d2c0a1a9cca6e2d863883d2cc
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-10-08 12:45:34 -0700

    Tasks 1-4.2.3 done

---
+++ my-app/src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css';
+import ToggleTheme, { ClickCounter } from './hooksExercise';
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+import { dummyNotesList } from "./constant"; // Import the dummyNotesList from the appropriate module
+
+function App() {
+ return (
+   <div className='app-container'>
+    <div>
+      <ToggleTheme></ToggleTheme>
+    </div>
+    <form className="note-form">
+       <div><input placeholder="Note Title"></input></div>
+
+       <div><textarea></textarea></div>
+
+       <div><button type="submit">Create Note</button></div>
+    </form>
+    <div className="notes-grid">
+       {dummyNotesList.map((note) => (
+         <div
+           key={note.id}
+           className="note-item">
+           <div className="notes-header">
+             <button>x</button>
+           </div>
+           <h2> {note.title} </h2>
+           <p> {note.content} </p>
+           <p> {note.label} </p>
+         </div>
+       ))}
+     </div>
+   </div>
+
+ );
+}
+
+export default App;
